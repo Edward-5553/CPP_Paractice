8672921950a7078a0ec86d668c6b05d049128585
EdwardYang <EdwardYang@ami.com>
2025-12-04 15:55:07 +0800

    feat: 添加计算矩形面积的程序
    实现一个简单的控制台程序，用于接收用户输入的长度

---
+++ 1_19_rectangle_area.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  double length, width;
+
+  cout << "Please input the length for rectangle: " << endl;
+  cin >> length;
+
+  cout << "Please input the width for rectangle: " << endl;
+  cin >> width;
+
+  cout << "The area for the rectangle : " << length * width << endl;
+
+  return 0;
+}
